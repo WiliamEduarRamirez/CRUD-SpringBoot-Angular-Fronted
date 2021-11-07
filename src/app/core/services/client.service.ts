19e3e1ba93315d37c31328d1de14165f6fd9a9a2
WiliamRamirez <wiliam.unsch@gmail.com>
2021-11-06 23:43:06 -0500

    initial commit

--- src/app/core/services/client.service.ts
+++ src/app/core/services/client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Client } from '../../shared/models/client';
 
@@ -8,10 +8,16 @@ import { Client } from '../../shared/models/client';
 })
 export class ClientService {
    private urlEndPoint = 'http://localhost:8080/api';
+   private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
    constructor(private http: HttpClient) {}
 
    getClients(): Observable<Client[]> {
       return this.http.get<Client[]>(`${this.urlEndPoint}/clients`);
+   }
+   create(client: Client): Observable<Client> {
+      return this.http.post<Client>(`${this.urlEndPoint}/clients`, client, {
+         headers: this.httpHeaders,
+      });
    }
 }
--- src/app/modules/clients/components/client-form/client-form.component.ts
+++ src/app/modules/clients/components/client-form/client-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '../../../../shared/models/client';
+import { ClientService } from '../../../../core/services/client.service';
+import { Router } from '@angular/router';
 
 @Component({
    selector: 'app-client-form',
@@ -8,12 +10,14 @@ import { Client } from '../../../../shared/models/client';
 export class ClientFormComponent implements OnInit {
    client: Client = new Client();
    title = 'Crear Cliente';
-   constructor() {}
+   constructor(private clientService: ClientService, private router: Router) {}
 
    ngOnInit(): void {}
 
    create(): void {
-      console.log('entra aqui');
-      console.log(this.client);
+      this.clientService.create(this.client).subscribe((response) => {
+         console.log(response);
+         this.router.navigate(['/clients']);
+      });
    }
 }
